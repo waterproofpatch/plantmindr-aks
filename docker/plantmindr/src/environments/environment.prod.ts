b6d8df1b50e17fa0b5bee42cbaa7983648a89f87
waterproofpatch <waterproofpatch@gmail.com>
2024-11-14 15:29:51 -0500

    notes

--- docker/plantmindr/src/environments/environment.prod.ts
+++ docker/plantmindr/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+  // TODO: change to point to aks cluster frontend
   apiUrlBase: 'https://strider.azurewebsites.net',
+  // TODO: change to point to aks cluster backend
   wsUrl: 'wss://strider.azurewebsites.net/ws',
 };
