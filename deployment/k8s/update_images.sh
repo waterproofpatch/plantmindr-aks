152462d0d65e5092fb0b91bd56716392eb503aa0
waterproofpatch <waterproofpatch@gmail.com>
2024-11-19 11:44:35 -0500

    updat images refactor

--- deployment/k8s/update_images.sh
+++ deployment/k8s/update_images.sh
@@ -1,8 +1,27 @@
+#!/bin/bash
+
+# Parameters
 RG_NAME="akswebapprg"
 ACRNAME="mcoronistestacr3"
+IMAGE_NAME=$1
 
-# acr steps
-# az acr build --registry $ACRNAME --image aks-test/test_frontend:latest ../../docker/frontend/
-az acr build --registry $ACRNAME --image aks-test/plantmindr-frontend:latest ../../docker/plantmindr/
-az acr build --registry $ACRNAME --image aks-test/plantmindr-backend:latest ../../docker/backend/
+if [ -z "$IMAGE_NAME" ]; then
+  echo "Usage: $0 <image_name>"
+  exit 1
+fi
+
+# ACR steps
+case $IMAGE_NAME in
+  frontend)
+    az acr build --registry $ACRNAME --image aks-test/plantmindr-frontend:latest ../../docker/plantmindr/
+    ;;
+  backend)
+    az acr build --registry $ACRNAME --image aks-test/plantmindr-backend:latest ../../docker/backend/
+    ;;
+  *)
+    echo "Unknown image name: $IMAGE_NAME. Valid options are 'frontend' or 'backend'."
+    exit 1
+    ;;
+esac
+
 az acr repository list --name $ACRNAME --output table
