c79bb94a04a3b265fa372d706f05df5af41947ba
waterproofpatch <waterproofpatch@gmail.com>
2025-03-24 11:07:01 -0400

    remove db deploy targets

--- deployment/azure/deploy_db.sh
+++
@@ -1,2 +0,0 @@
-# az deployment group create --resource-group "akswebapprg" --template-file db.bicep --parameters serverName="plantmindrdbserver2" adminUser="adminUser" adminPassword="adminPassword" location="centralus"
-
