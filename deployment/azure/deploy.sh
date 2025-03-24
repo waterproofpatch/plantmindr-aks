6c83a24fbc2269172585fe9ed16e40272ecf5b99
waterproofpatch <waterproofpatch@gmail.com>
2025-03-24 11:27:10 -0400

    refactor

--- deployment/azure/deploy.sh
+++ deployment/azure/deploy.sh
@@ -1,22 +1,21 @@
 RG_NAME="akswebapprg"
 CLUSTER_NAME="webappakscluster"
 LOCATION="eastus"
-DEPLOY_BICEP="rg.bicep"
-AKS_BICEP="aks.bicep"
 KEY_NAME="sshKey"
 DNS_PREFIX="mcoronistestaks"
 USER="azureuser"
 ACRNAME="mcoronistestacr3"
 
-# resource group (subscription level)
-# az group create --name $RG_NAME --location $LOCATION
+RG_DEPLOY_BICEP="rg.bicep"
+AKS_DEPLOY_BICEP="aks.bicep"
+
 az login
 
-echo "Creating deployment..."
+echo "Creating RG deployment..."
 az deployment sub create \
     --name testdeployment \
     --location $LOCATION \
-    --template-file $DEPLOY_BICEP \
+    --template-file $RG_DEPLOY_BICEP \
     --parameters resourceGroupName=$RG_NAME resourceGroupLocation=$LOCATION
 
 # Create an SSH key pair using Azure CLI. Fails noncrit if key exists
@@ -24,10 +23,10 @@ az sshkey create --name "$KEY_NAME" --resource-group $RG_NAME
 PUBLIC_KEY=$(az sshkey show --name "$KEY_NAME" --resource-group $RG_NAME --query "publicKey" --output tsv)
 
 # # aks cluster (rg level)
-echo "Creating AKS cluster with $PUBLIC_KEY"
+echo "Creating AKS deployment..."
 az deployment group create \
     --resource-group $RG_NAME \
-    --template-file $AKS_BICEP \
+    --template-file $AKS_DEPLOY_BICEP \
     --parameters \
     clusterName=$CLUSTER_NAME \
     dnsPrefix=$DNS_PREFIX \
