c22c8f45a290c2fd91163299cfacc65295ed725d
waterproofpatch <waterproofpatch@gmail.com>
2025-03-30 15:50:48 -0400

    rev

--- oldplants/syncdb.go
+++ oldplants/syncdb.go
@@ -17,20 +17,22 @@ func main() {
 	srcDbPassword := flag.String("srcDbPassword", "srcPassword", "Source DB Password")
 	srcDbUrlName := flag.String("srcDbUrlName", "srcName", "Source DB URL name")
 	srcDbName := flag.String("srcDbName", "srcDbName", "Source DB name")
+	srcDbUsername := flag.String("srcDbUsername", "srcDbUsername", "Source DB username")
 	dstDbPassword := flag.String("dstDbPassword", "dstPassword", "Destination DB Password")
 	dstDbUrlName := flag.String("dstDbUrlName", "dstName", "Destination DB URL name")
 	dstDbName := flag.String("dstDbName", "dstDbName", "Destination DB name")
+	dstDbUsername := flag.String("dstDbUsername", "dstDbUsername", "Destination DB username")
 	flag.Parse()
 
 	// parse the password from each database from arguments
-	postgresDSN := fmt.Sprintf("host=%s.postgres.database.azure.com user=freeman password=%s dbname=%s port=5432 sslmode=require", *srcDbUrlName, *srcDbPassword, *srcDbName)
+	postgresDSN := fmt.Sprintf("host=%s.postgres.database.azure.com user=%s password=%s dbname=%s port=5432 sslmode=require", *srcDbUrlName, *srcDbUsername, *srcDbPassword, *srcDbName)
 	sourceDb, err := gorm.Open(postgres.Open(postgresDSN), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to PostgreSQL: %v", err)
 	}
 
 	// Connect to Azure SQL database
-	azureSQLDSN := fmt.Sprintf("server=%s.database.windows.net;user id=plantmindr;password=%s;port=1433;database=%s;", *dstDbUrlName, *dstDbPassword, *dstDbName)
+	azureSQLDSN := fmt.Sprintf("server=%s.database.windows.net;user id=%s;password=%s;port=1433;database=%s;", *dstDbUrlName, *dstDbUsername, *dstDbPassword, *dstDbName)
 	targetDb, err := gorm.Open(sqlserver.Open(azureSQLDSN), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to Azure SQL: %v", err)
