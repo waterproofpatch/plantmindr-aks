a1f2919b7b41c477fc9df66e1802fb9cdbf69a52
waterproofpatch <waterproofpatch@gmail.com>
2025-03-30 10:55:01 -0400

    more args

--- oldplants/syncdb.go
+++ oldplants/syncdb.go
@@ -15,31 +15,28 @@ import (
 
 func main() {
 	srcDbPassword := flag.String("srcDbPassword", "srcPassword", "Source DB Password")
-	srcDbName := flag.String("srcDbName", "srcName", "Source DB name (postgres)")
+	srcDbUrlName := flag.String("srcDbUrlName", "srcName", "Source DB URL name")
+	srcDbName := flag.String("srcDbName", "srcDbName", "Source DB name")
 	dstDbPassword := flag.String("dstDbPassword", "dstPassword", "Destination DB Password")
-	dstDbName := flag.String("dstDbName", "dstName", "Destination DB name (postgres)")
+	dstDbUrlName := flag.String("dstDbUrlName", "dstName", "Destination DB URL name")
+	dstDbName := flag.String("dstDbName", "dstDbName", "Destination DB name")
 	flag.Parse()
-	fmt.Printf("Source DB Password: %s\n", *srcDbPassword)
-	fmt.Printf("Destination DB Password: %s\n", *dstDbPassword)
-	// return
 
 	// parse the password from each database from arguments
-	postgresDSN := fmt.Sprintf("host=%s.postgres.database.azure.com user=freeman password=%s dbname=postgres port=5432 sslmode=require", *srcDbName, *srcDbPassword)
+	postgresDSN := fmt.Sprintf("host=%s.postgres.database.azure.com user=freeman password=%s dbname=%s port=5432 sslmode=require", *srcDbUrlName, *srcDbPassword, *srcDbName)
 	sourceDb, err := gorm.Open(postgres.Open(postgresDSN), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to PostgreSQL: %v", err)
 	}
 
 	// Connect to Azure SQL database
-	azureSQLDSN := fmt.Sprintf("server=%s.database.windows.net;user id=plantmindr;password=%s;port=1433;database=plantmindersqldb;", *dstDbName, *dstDbPassword)
+	azureSQLDSN := fmt.Sprintf("server=%s.database.windows.net;user id=plantmindr;password=%s;port=1433;database=%s;", *dstDbUrlName, *dstDbPassword, *dstDbName)
 	targetDb, err := gorm.Open(sqlserver.Open(azureSQLDSN), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to Azure SQL: %v", err)
 	}
 
 	fmt.Println("Connected to PostgreSQL and Azure SQL databases successfully!")
-
-	fmt.Println("Starting synchronization...")
 
 	fmt.Println("Migrating PlantModel...")
 	// AutoMigrate the schema for Azure SQL
